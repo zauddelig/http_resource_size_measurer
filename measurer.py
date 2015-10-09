4200e3d5b78624a53c929d228f680b99dac9704b
Fabrizio Ettore Messina <zauddelig@gmail.com>
2015-10-09 23:32:13 +0200

    Fabrizio: Added some unit testing, the unit tests over the links parser is pretty comprehensive over the scope of the application, whilst the linkparser itself is not fully compliant for the query inheritance, feature that I should study a little more. Enhanched the Command line tools, the vanilla reply is quite simple added the -T parameter to print all the request tree details.

--- measurer.py
+++ measurer.py
@@ -6,6 +6,8 @@ from multiprocessing import Pool, Lock
 from html.parser import HTMLParser
 import argparse
 import logging
+import re
+from time import time
 
 class WrongCondition(Exception):
     pass
@@ -19,12 +21,50 @@ class LinksGetter(HTMLParser):
     }
     links = None
     url = ""
-    
+    regex = re.compile("^(https?:)//")
     def __init__(self, url, *args, **kwargs):
         super(LinksGetter, self).__init__(*args, **kwargs)
         self.url = url
         self.links = []
-      
+
+    @staticmethod
+    def parse_dotted_path(link):
+        """
+        This function takes a string with unix like dots:
+        "first/../second"
+        and return the effective path
+        "second"
+        """
+        splitted = link.split("/")
+        while True:
+            try:
+                index = splitted.index("..") - 1
+            except ValueError:
+                break
+            if index <= 0:
+                return None
+            splitted.pop(index)
+            splitted.pop(index)
+        link = "/".join(splitted)
+        return link
+
+    def parse_relative_url(self, link):
+        """
+            partial implementation of http://www.ietf.org/rfc/rfc1808.txt missing query inheritance
+        """
+        match = self.regex.search(self.url)
+        protocol = match.group(0)
+        url = self.url[len(protocol):]
+        url = url[-1] == "/" and url[:-1] or url
+        if link.startswith("//"):
+            link = protocol[:-2] + link
+        elif link.startswith("/"):
+            domain = url.split("/")[0]
+            link = protocol + self.parse_dotted_path(domain + link)
+        else:
+            link = protocol + self.parse_dotted_path(url + "/" + link)
+        return link
+
     def handle_starttag(self, tag, attrs):
         attrs = dict(attrs)
         try:
@@ -35,21 +75,19 @@ class LinksGetter(HTMLParser):
                     raise WrongCondition
         except (KeyError, WrongCondition):
             return
-        else:
-            if not link.startswith("http"):
-                    # note we are not parsing the base url so it will not always work
-                    link = self.url + link
-            self.links.append(link)
+
+        if not self.regex.search(link):
+            link = self.parse_relative_url(link)
+        self.links.append(link)
 
 FETCHED_URLS = set()
 
 def create_resource(link):
     with Lock():
-        if link in FETCHED_URLS:
+        if not link or link in FETCHED_URLS:
             return None
         FETCHED_URLS.add(link)
     resource = Resource(link)
-    resource.data  # we cannot do it lazy here because the BufferReader is not pickable
     return resource
 
 class Resource():
@@ -58,25 +96,32 @@ class Resource():
     url = ""
     request = None
     response = None
+    time = 0
 
     _resources = None
     _data = None
     _size = None
-
     _processes = 0
 
     def __init__(self, url, processes=0):
+        if not url.startswith("http"):
+            url = "http://" + url
         self.url = url
         FETCHED_URLS.add(url)
         self.request = request.Request(url)
         self._processes = processes
         try:
-            self.response = request.urlopen(self.request)
-            self.data
+            self._make_request()
             logging.info("url successfull: " + url)
         except urllib.error.HTTPError:
             logging.error("url failed: " + url)
             self._empty()
+
+    def _make_request(self):
+        start = time()
+        self.response = request.urlopen(self.request)
+        self.data
+        self.time = time() - start
 
     def _empty(self):
         """
@@ -88,7 +133,7 @@ class Resource():
 
 
     def _get_links(self, html):
-        parser = LinksGetter(self.url, str(html))
+        parser = LinksGetter(self.url)
         parser.feed(str(html))
         return parser.links
 
@@ -139,11 +184,18 @@ class Resource():
         """
             Return the resources pyramid iterator
         """
-        yield self
+        yield (self, )
         for resource in self.resources:
             for sub_resource in resource.flat_resources_tree():
                 yield self, sub_resource
-
+    def get_total_time(self):
+        """
+            Return the total time of the resources tree
+        """
+        time = 0
+        for resource in self.flat_resources_tree():
+            time += resource.time
+        return time
     def get_total_size(self):
         """
             Return the total size of the resources tree
@@ -156,20 +208,27 @@ class Resource():
     def get_links_count(self):
         return len(list(self.resources))
 
-    def __repr__(self):
-        return self.url
+    def __str__(self):
+        return "%s,\t%ss,\t%sB" %(self.url, self.time, self.size)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("url")
+    parser.add_argument("url", help="The url you want to study")
     parser.add_argument(
         "-p", "--processes", help="Number of processes to spawn for asyncronous url getter", type=int, default=0,
+    )
+    parser.add_argument(
+        "-T", "--tree", action='store_true',
+        help="Print the resources tree, each resource is represented by a tuple of url, time, size",
     )
     args = parser.parse_args()
     url = args.url
     processes = args.processes
     main_resource = Resource(url, processes)
-    print(main_resource.size)
-    print(main_resource.get_total_size())
+    if args.tree:
+        for branch in main_resource.piramid_tree():
+            point, branch= branch[-1], branch[:-1]
+            print("".join("\t" for e in branch) + str(point))
+    print("Total size:\t%sB\nTotal time:\t%ss" % (main_resource.get_total_size(), main_resource.get_total_time()))
 
---
+++ test.py
@@ -0,0 +1,91 @@
+import unittest
+import multiprocessing
+from measurer import LinksGetter, Resource
+from wsgiref.util import setup_testing_defaults
+from wsgiref.simple_server import make_server
+
+
+class TestLinksGetter(unittest.TestCase):
+	main_url = "http://father.test/"
+
+	def get_feed(self, html, url=None):
+		if not url:
+			url = self.main_url
+		parser = LinksGetter(url)
+		parser.feed(html)
+		return parser
+
+	def test_iframe(self):
+		html = "<hmtl><iframe src='http://example.com'></iframe></html>"
+		parser = self.get_feed(html)
+		self.assertEqual(parser.links, ["http://example.com"])
+
+	def test_css_link(self):
+		html = "<hmtl><link rel='stylesheet' href='http://example.com/test.css'></iframe></html>"
+		parser = self.get_feed(html, self.main_url+"test/")
+		self.assertEqual(parser.links, ["http://example.com/test.css"])
+
+	def test_ignore_most_links(self):
+		html = "<hmtl><link href='http://example.com/test.css'></iframe></html>"
+		parser = self.get_feed(html, self.main_url+"test/")
+		self.assertEqual(parser.links, [])
+
+	def test_image(self):
+		html = "<hmtl><img src='http://example.com/test.png'></script></html>"
+		parser = self.get_feed(html, self.main_url+"test/")
+		self.assertEqual(parser.links, ["http://example.com/test.png"])
+
+	def test_script(self):
+		html = "<hmtl><script src='http://example.com/test.js'></script></html>"
+		parser = self.get_feed(html, self.main_url+"test/")
+		self.assertEqual(parser.links, ["http://example.com/test.js"])
+
+	def test_relative_link(self):
+		html = "<hmtl><link rel='stylesheet' href='/test.css'></link></html>"
+		parser = self.get_feed(html, self.main_url+"test/")
+		self.assertEqual(parser.links, [self.main_url+"test.css"])
+
+	def test_absolute_link(self):
+		html = "<hmtl><link rel='stylesheet' href='/test.css'></link></html>"
+		parser = self.get_feed(html, self.main_url+"test/")
+		self.assertEqual(parser.links, [self.main_url+"test.css"])
+
+	def test_net_link(self):
+		html = "<hmtl><link rel='stylesheet' href='//example.com/test.css'></link></html>"
+		parser = self.get_feed(html, "https://father.test/test/")
+		self.assertEqual(parser.links, ["https://example.com/test.css"])
+
+	def test_dotted_relative_link(self):
+		html = "<hmtl><link rel='stylesheet' href='../example/test.css'></link></html>"
+		parser = self.get_feed(html, "https://father.test/test/")
+
+	def test_dotted_absolute_link(self):
+		html = "<hmtl><link rel='stylesheet' href='/example/../test.css'></link></html>"
+		parser = self.get_feed(html, "https://father.test/test/")
+		self.assertEqual(parser.links, ["https://father.test/test.css"])
+
+class TestResource(unittest.TestCase):
+	"""
+		for testing that we need a simple server on a custom port
+	"""
+
+	def test_simple(self):
+		"""
+		Note we assume that this page size is very unlikelly to change
+		:return:
+		"""
+		resource = Resource("http://example.com/")
+		self.assertEqual(resource.size, 1270)
+
+	def test_non_html(self):
+		resource = Resource("http://www.ietf.org/rfc/rfc1808.txt")
+		self.assertAlmostEqual(resource.size, 34130, delta=1000)  # had to give a rather big delta here
+		resource = Resource("http://www.governo.it/Governo/Costituzione/CostituzioneRepubblicaItaliana.pdf")
+		self.assertAlmostEqual(resource.size, 188249)
+
+
+
+
+
+if __name__=="__main__":
+	unittest.main()
